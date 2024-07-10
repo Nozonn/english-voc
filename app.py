01727bf87764083bc22435f7bf015f16ab03f905
Nozonn <125192809+Nozonn@users.noreply.github.com>
2024-07-10 17:29:13 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
 from flask import (Flask, render_template, request)
 
 
-host, port, isDebug = ('192.168.1.159', 8000, True)
+#host, port, isDebug = ('192.168.1.159', 8000, True)
 
-class App:
-    app = Flask(__name__)
-    def __init__(self) -> None:
-        pass
-    
+app = Flask(__name__)
 
-
-    @app.route('/')
-    def index():
-        return render_template("index.html")
-
-
-    def run(self, host, port, isDebug):
-        self.app.run(host=host, port=port, debug=isDebug)
-
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app = App()
-    app.run(host, port, isDebug)
+    #app.run(host=host, port=port, debug=isDebug)
+    app.run()
